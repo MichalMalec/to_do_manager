48999c486de588565613b8895bf20f0c03feb936
Michal <michalmalec5@o2.pl>
2020-01-26 16:55:46 +0100

    POST project endpoint test

--- spec/acceptance/projects_spec.rb
+++ spec/acceptance/projects_spec.rb
@@ -8,7 +8,6 @@ resource "Projects" do
     user = User.all.where(id: 1).first
     header "X-User-Token", user.authentication_token
     header "X-User-Email", user.email
-    header "Content-Type", "application/json"
   end
   
   get "/v1/projects" do
@@ -18,6 +17,31 @@ resource "Projects" do
 
       expect(status).to eq 200
       expect(parsed_response).to have_key("projects")
+    end
+  end
+
+  get "/v1/projects/:id" do
+    byebug
+    let(:project) { Project.create(name: 'TEST project') }
+    let(:id) { 11 }
+    example "POST project and then GET that project endpoints" do
+      do_request
+      parsed_response = JSON.parse(response_body)
+    end
+  end
+  
+  post "/v1/projects" do
+    example "POST project endpoint" do
+      request_body = { 
+        "project": {
+          "name": "POST project" 
+        }
+      }
+      do_request(request_body)
+      parsed_response = JSON.parse(response_body)
+      expect(status).to eq 201
+      expect(parsed_response).to have_key("project")
+      expect(parsed_response["project"].keys).to contain_exactly("id", "name")
     end
   end
 end
